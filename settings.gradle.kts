b428f4497885b0c3bd27e02060e8b3b916ab2a4e
brainsynder <briansnyder498@gmail.com>
2026-03-17 17:51:21 -0400

    refactor: remove support for version v1_21_6 in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ rootProject.name = "SimplePets"
 include(":api")
 include(":main")
 
-include(":versions:v1_21_6")
+// include(":versions:v1_21_6") // Removed support in version R5-B296
 include(":versions:v1_21_7")
 include(":versions:v1_21_8")
 include(":versions:v1_21_10")
